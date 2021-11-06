e6fff6e6d3a2bbad74157fa311e72267890bd25b
LanceStasinski <lance.stasinski@gmail.com>
2021-11-06 13:14:18 -0400

    style: Restyle navigation

--- client/src/shared/components/Navigation/MainNavigation.tsx
+++ client/src/shared/components/Navigation/MainNavigation.tsx
@@ -9,15 +9,15 @@ import classes from './MainNavigation.module.css'
 const MainNavigation: React.FC = () => {
   return (
     <MainHeader>
+      <h1 className={classes['main-title']}>
+        <Link to='/'>LANCE STASINSKI</Link>
+      </h1>
       <button className={classes['menu-btn']}>
         <span />
         <span />
         <span />
       </button>
-      <h1 className={classes['main-title']}>
-        <Link to='/'>Lance Stasinski</Link>
-      </h1>
-      <nav><NavLinks /></nav>
+      <nav className={classes['main-nav']}><NavLinks /></nav>
     </MainHeader>
   );
 };
