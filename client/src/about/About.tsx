0345d6ccf8e2879b727ac3bcf9a1ee635d93e3bd
LanceStasinski <lance.stasinski@gmail.com>
2021-11-07 10:50:04 -0500

    feat: Add text elements to about page

--- client/src/about/About.tsx
+++ client/src/about/About.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 
-// import classes from "./About.module.css";
+import classes from "./About.module.css";
 
 const About: React.FC = () => {
-  return <h1>About page</h1>;
+  return (
+    <div className={classes.about}>
+      <h2>Hello World</h2>
+      <p>
+        My name is Lance Stasinski and welcome to my personal website. I'm a
+        quantitative biologist turned web developer, and I have found a love for
+        building fullstack applications with elegant user interfaces. This site
+        contains information about me, the projects I've built so far, and a
+        blog where I explain concepts that I have found challenging while
+        learning to code.
+      </p>
+    </div>
+  );
 };
 
 export default About;
