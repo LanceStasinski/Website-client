0f5134b046ce7d07143ffdda0b62fc7c02bf7ea2
LanceStasinski <lance.stasinski@gmail.com>
2021-11-10 15:22:52 -0500

    test: Add NavLinks test

--- client/src/shared/components/Navigation/NavLinks.test.tsx
+++ client/src/shared/components/Navigation/NavLinks.test.tsx
@@ -2,26 +2,26 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter, Route } from "react-router-dom";
 
-import DrawerNavLinks from "./DrawerNavLinks";
+import NavLinks from "./NavLinks";
 
-describe("DrawerNavLinks component", () => {
+describe("NavLinks component", () => {
   test("renders cv link", () => {
-    render(<BrowserRouter><Route><DrawerNavLinks /></Route></BrowserRouter>);
+    render(<BrowserRouter><Route><NavLinks /></Route></BrowserRouter>);
     const CVLink = screen.getByText(/cv/i);
     expect(CVLink).toBeInTheDocument();
   });
   test("renders portfolio link", () => {
-    render(<BrowserRouter><Route><DrawerNavLinks /></Route></BrowserRouter>);
+    render(<BrowserRouter><Route><NavLinks /></Route></BrowserRouter>);
     const CVLink = screen.getByText(/portfolio/i);
     expect(CVLink).toBeInTheDocument();
   });
   test("renders blog link", () => {
-    render(<BrowserRouter><Route><DrawerNavLinks /></Route></BrowserRouter>);
+    render(<BrowserRouter><Route><NavLinks /></Route></BrowserRouter>);
     const CVLink = screen.getByText(/blog/i);
     expect(CVLink).toBeInTheDocument();
   });
   test("renders contact link", () => {
-    render(<BrowserRouter><Route><DrawerNavLinks /></Route></BrowserRouter>);
+    render(<BrowserRouter><Route><NavLinks /></Route></BrowserRouter>);
     const CVLink = screen.getByText(/contact/i);
     expect(CVLink).toBeInTheDocument();
   });
