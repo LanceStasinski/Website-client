0b301ada9b32e5a87b02e7f2b36270f93fbb0700
LanceStasinski <lance.stasinski@gmail.com>
2021-11-10 14:06:07 -0500

    feat: Create mobile layout for about page

--- client/src/about/About.tsx
+++ client/src/about/About.tsx
@@ -7,11 +7,16 @@ import backgroundImage from "../assets/about-background.JPG";
 import githubIcon from "../assets/social-icons/github.png";
 import linkedinIcon from "../assets/social-icons/linkedin.png";
 import twitterIcon from "../assets/social-icons/twitter.png";
+import headshot from "../assets/headshot-3.jpg";
 
 const About: React.FC = () => {
   return (
     <React.Fragment>
+      <div className={classes.headshot}>
+        <img src={headshot} alt="Lance Stasinski" />
+      </div>
       <div
+        className={classes["background-image"]}
         style={{
           backgroundImage: `url(${backgroundImage})`,
           backgroundRepeat: "no-repeat",
@@ -34,15 +39,13 @@ const About: React.FC = () => {
           in the R statistical computing language. Since then, I have
           transitioned my programming expertise to building full stack web
           applications, and I have found a passion for building elegant user
-          interfaces along the way. This site contains information about me, the
-          projects I've built so far, and a blog where I explain concepts that I
-          have found challenging while learning to code.
+          interfaces along the way.
         </p>
         <p>
           While you're here, feel free to peruse my CV and resume, explore and
           test my portfolio projects, or read my blog that details concepts that
           I have found challenging and have overcome. You can get in touch with
-          me by visiting the CONTACT page or you can connect with me over social
+          me by visiting the contact page or you can connect with me over social
           media linked below.
         </p>
         <ul className={classes["social-links"]}>
--- client/src/custom.d.ts
+++ client/src/custom.d.ts
@@ -1,2 +1,3 @@
 declare module "*.JPG"
+declare module "*.jpg"
 declare module "*.png"
