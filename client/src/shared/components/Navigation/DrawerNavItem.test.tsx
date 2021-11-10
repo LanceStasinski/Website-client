0ce66a6d4397175903455e3aa3eacc7082e11e06
LanceStasinski <lance.stasinski@gmail.com>
2021-11-10 15:19:43 -0500

    test: Add DrawerNavLinks test

--- client/src/shared/components/Navigation/DrawerNavItem.test.tsx
+++ client/src/shared/components/Navigation/DrawerNavItem.test.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import DrawerNavItem from "./DrawerNavItem";
 import { BrowserRouter, Route } from "react-router-dom";
 
-test("renders learn react link", () => {
+import DrawerNavItem from "./DrawerNavItem";
+
+
+test("renders a link", () => {
   render(
     <BrowserRouter>
       <Route>
