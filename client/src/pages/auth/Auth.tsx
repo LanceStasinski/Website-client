e1d441e0942215fb642bd8a8837439f25b770f3c
LanceStasinski <lance.stasinski@gmail.com>
2021-11-15 15:38:23 -0500

    feat: Add login validation

--- client/src/pages/auth/Auth.tsx
+++ client/src/pages/auth/Auth.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 import classes from "./Auth.module.css";
@@ -14,15 +14,16 @@ interface LoginInput {
 const Auth: React.FC = () => {
   const {
     register,
-    formState: { errors },
+    formState: { errors, isValid },
     handleSubmit,
-  } = useForm();
+  } = useForm({ mode: "onChange" });
+
   const onSubmit: SubmitHandler<LoginInput> = (data) => console.log(data);
   return (
     <React.Fragment>
       <div className={classes.auth}>
         <Card className={classes["auth-card"]}>
-          <div className={classes['auth-card-header']}>
+          <div className={classes["auth-card-header"]}>
             <h2>LOGIN</h2>
           </div>
           <form
@@ -30,11 +31,25 @@ const Auth: React.FC = () => {
             className={classes["auth-form"]}
           >
             <label htmlFor="username">Username</label>
-            <input {...register("username", { required: true })} />
-            {errors.username && "Username required."}
+            <input
+              {...register("username", { required: true })}
+              className={errors.username && classes["input-error"]}
+            />
+            {errors.username && <p>Username required.</p>}
             <label htmlFor="password">Password</label>
-            <input id="password" type='password'/>
-            <Button type="submit" onClick={onSubmit}>
+            <input
+              id="password"
+              type="password"
+              {...register("password", { required: true, minLength: 6 })}
+              className={errors.username && classes["input-error"]}
+            />
+            {errors.password && <p>Password must be at least 6 characters.</p>}
+
+            <Button
+              type="submit"
+              onClick={onSubmit}
+              disabled={!isValid}
+            >
               submit
             </Button>
           </form>
