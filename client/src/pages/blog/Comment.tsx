51b88bb590bb8e7cb0ef3b8af25dc4648bcad742
LanceStasinski <lance.stasinski@gmail.com>
2021-12-08 14:08:25 -0500

    feat: Add ability to delete comment

--- client/src/pages/blog/Comment.tsx
+++ client/src/pages/blog/Comment.tsx
@@ -2,6 +2,11 @@ import React, { useContext, useState } from "react";
 
 import { AuthContext } from "../../shared/context/auth-context";
 import classes from "./Comment.module.css";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+
+const REST_API = process.env.REACT_APP_REST_API;
 
 interface Props {
   userId: string;
@@ -11,41 +16,39 @@ interface Props {
 
 const Comment: React.FC<Props> = (props) => {
   const authCtx = useContext(AuthContext);
-  const [isEditing, setIsEditing] = useState(false);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
-  const editHandler = () => {
-    setIsEditing(true);
+  const deleteCommentHandler = async () => {
+    try {
+      await sendRequest(
+        `${REST_API}/blog/comment/${props.commentId}`,
+        "DELETE",
+        {},
+        { Authorization: "Bearer " + authCtx.token }
+      );
+    } catch (error) {}
   };
 
-  const cancelEditHandler = () => {
-    setIsEditing(false);
-  };
-
-  const updateCommentHandler = () => {
-    //post request
-  };
-
-  const deleteCommentHandler = () => {
-    //DELETE request
-  };
-
-  // make sure userId matches userId fetched
   return (
-    <div className={classes['comment-wrapper']}>
-      <div className={classes.comment}>
-        <div className={classes["comment-single"]}>
-          <h3>{props.userName}</h3>
-          <p>{props.children}</p>
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {/* {isLoading && <LoadingSpinner asOverlay={true} />} */}
+      <div className={classes["comment-wrapper"]}>
+        <div className={classes.comment}>
+          <div className={classes["comment-single"]}>
+            <h3>{props.userName}</h3>
+            <p>{props.children}</p>
+          </div>
         </div>
+        {authCtx.isLoggedIn && props.userId === authCtx.userId && (
+          <div className={classes["delete-btn"]}>
+            <button type="button" onClick={deleteCommentHandler}>
+              Delete
+            </button>
+          </div>
+        )}
       </div>
-      {authCtx.isLoggedIn && props.userId === authCtx.userId && (
-        <div className={classes['delete-btn']}>
-          <button type="button" onClick={deleteCommentHandler}>
-            Delete
-          </button>
-        </div>
-      )}
-    </div>
+    </React.Fragment>
   );
 };
 
