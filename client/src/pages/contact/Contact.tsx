045d4b19e08efe63586939e14898e568a4b79a57
LanceStasinski <lance.stasinski@gmail.com>
2021-12-19 10:54:58 -0500

    style: Style contact form

--- client/src/pages/contact/Contact.tsx
+++ client/src/pages/contact/Contact.tsx
@@ -17,31 +17,50 @@ const Contact: React.FC = () => {
     register,
     formState: { errors, isValid },
     handleSubmit,
-    watch,
   } = useForm({ mode: "onChange" });
 
   const submitFormHandler = async (userData: ContactInput) => {};
   const onSubmit: SubmitHandler<ContactInput> = (data) =>
     submitFormHandler(data);
   return (
-    <Card>
-      <h2>Contact me</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="first-name">First Name: </label>
-        <input type="text" id="firstName" {...register('firstName', {required: true})}/>
-        {errors.firstName && <p>Please provide your first name.</p>}
-        <label htmlFor="last-name">Last Name: </label>
-        <input type="text" id="lastName" {...register('lastName', {required: true})} />
-        {errors.lastName && <p>Please provide your last name.</p>}
-        <label htmlFor="email">Email: </label>
-        <input id="email" type='email' {...register('email', {required: true})} />
-        {errors.email && <p>Please provide your email.</p>}
-        <label htmlFor="message">Message: </label>
-        <textarea id="message" {...register('message', {required: true})} />
-        {errors.message && <p>Please provide a message.</p>}
-        <Button type="submit" disabled={!isValid}>SEND</Button>
-      </form>
-    </Card>
+    <div className={classes["contact-wrapper"]}>
+      <Card className={classes["contact-card"]}>
+        <div className={classes["contact-card-header"]}>
+          <h2>Contact me</h2>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="first-name">First Name</label>
+          <input
+            type="text"
+            id="firstName"
+            {...register("firstName", { required: true })}
+          />
+          {errors.firstName && <p>Please provide your first name.</p>}
+          <label htmlFor="last-name">Last Name</label>
+          <input
+            type="text"
+            id="lastName"
+            {...register("lastName", { required: true })}
+          />
+          {errors.lastName && <p>Please provide your last name.</p>}
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            {...register("email", { required: true })}
+          />
+          {errors.email && <p>Please provide your email.</p>}
+          <label htmlFor="message">Message</label>
+          <textarea id="message" {...register("message", { required: true })} />
+          {errors.message && <p>Please provide a message.</p>}
+
+            <Button type="submit" disabled={!isValid}>
+              SEND
+            </Button>
+
+        </form>
+      </Card>
+    </div>
   );
 };
 
