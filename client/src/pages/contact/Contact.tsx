f154790ca88e81ecffd3e49e663367c56d6f9099
LanceStasinski <lance.stasinski@gmail.com>
2021-12-19 10:17:43 -0500

    feat: Add form validtation to contact form

--- client/src/pages/contact/Contact.tsx
+++ client/src/pages/contact/Contact.tsx
@@ -1,25 +1,48 @@
 import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 
-import classes from './Contact.module.css';
+import classes from "./Contact.module.css";
 import Button from "../../shared/components/FormElements/Button";
+import Card from "../../shared/components/UIElements/Card";
+
+interface ContactInput {
+  first: string;
+  last: string;
+  email: string;
+  message: string;
+}
 
 const Contact: React.FC = () => {
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    watch,
+  } = useForm({ mode: "onChange" });
+
+  const submitFormHandler = async (userData: ContactInput) => {};
+  const onSubmit: SubmitHandler<ContactInput> = (data) =>
+    submitFormHandler(data);
   return (
-    <div>
+    <Card>
       <h2>Contact me</h2>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="first-name">First Name: </label>
-        <input type='text' id='first-name' name='first-name' />
+        <input type="text" id="firstName" {...register('firstName', {required: true})}/>
+        {errors.firstName && <p>Please provide your first name.</p>}
         <label htmlFor="last-name">Last Name: </label>
-        <input type='text' id="last-name" name="last-name" />
+        <input type="text" id="lastName" {...register('lastName', {required: true})} />
+        {errors.lastName && <p>Please provide your last name.</p>}
         <label htmlFor="email">Email: </label>
-        <input id="email" type='text' name='email' />
+        <input id="email" type='email' {...register('email', {required: true})} />
+        {errors.email && <p>Please provide your email.</p>}
         <label htmlFor="message">Message: </label>
-        <textarea id='message' name="message" />
-        <Button type='submit' />
+        <textarea id="message" {...register('message', {required: true})} />
+        {errors.message && <p>Please provide a message.</p>}
+        <Button type="submit" disabled={!isValid}>SEND</Button>
       </form>
-    </div>
-  )
-}
+    </Card>
+  );
+};
 
 export default Contact;
