7d61be70765227bcc2d7d93a2f53d17dbcf013f8
LanceStasinski <lance.stasinski@gmail.com>
2021-12-21 13:58:57 -0500

    feat: Add links to social media to contact page

--- client/src/pages/contact/Contact.tsx
+++ client/src/pages/contact/Contact.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { CSSTransition } from "react-transition-group";
 
 import classes from "./Contact.module.css";
 import Button from "../../shared/components/FormElements/Button";
@@ -20,12 +21,14 @@ const REST_API = process.env.REACT_APP_REST_API;
 
 const Contact: React.FC = () => {
   const [showMessageSent, setShowMessageSent] = useState(false);
+  const [showForm, setShowForm] = useState(false);
   const { isLoading, error, clearError, sendRequest } = useHttpClient();
+  const nodeRef = useRef(null);
   const {
     register,
     formState: { errors, isValid },
     handleSubmit,
-    reset
+    reset,
   } = useForm({ mode: "onChange" });
 
   const submitFormHandler = async (userData: ContactInput) => {
@@ -45,13 +48,17 @@ const Contact: React.FC = () => {
       );
       if (response.message === "Message recieved") {
         setShowMessageSent(true);
-        reset({firstName: '', lastName: '', email: '', message: ''})
+        reset({ firstName: "", lastName: "", email: "", message: "" });
       }
     } catch (error) {}
   };
 
   const closeModalHandler = () => {
     setShowMessageSent(false);
+  };
+
+  const showFormHandler = () => {
+    setShowForm((prevState) => !prevState);
   };
 
   const onSubmit: SubmitHandler<ContactInput> = (data) =>
@@ -65,59 +72,102 @@ const Contact: React.FC = () => {
         onCancel={closeModalHandler}
         header="Message Sent!"
         footer={
-          <div className={classes['okay']}>
-            <Button type="button" onClick={closeModalHandler}>OK</Button>
+          <div className={classes["okay"]}>
+            <Button type="button" onClick={closeModalHandler}>
+              OK
+            </Button>
           </div>
         }
-        backdropClass={classes['modal-backdrop']}
+        backdropClass={classes["modal-backdrop"]}
         // contentClass={classes['modal-content']}
       >
         Your message has been recieved. Thank you!
       </Modal>
       <div className={classes["contact-wrapper"]}>
         {isLoading && <LoadingSpinner asOverlay />}
-        <Card className={classes["contact-card"]}>
-          <div className={classes["contact-card-header"]}>
-            <h2>Send Me A Message</h2>
-          </div>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="firstName">First Name</label>
-            <input
-              type="text"
-              id="firstName"
-              {...register("firstName", { required: true })}
-              className={errors.firstName && classes["input-error"]}
-            />
-            {errors.firstName && <p>Please provide your first name.</p>}
-            <label htmlFor="lastName">Last Name</label>
-            <input
-              type="text"
-              id="lastName"
-              {...register("lastName", { required: true })}
-              className={errors.lastName && classes["input-error"]}
-            />
-            {errors.lastName && <p>Please provide your last name.</p>}
-            <label htmlFor="email">Email</label>
-            <input
-              id="email"
-              type="email"
-              {...register("email", { required: true })}
-              className={errors.email && classes["input-error"]}
-            />
-            {errors.email && <p>Please provide your email.</p>}
-            <label htmlFor="message">Message</label>
-            <textarea
-              id="message"
-              {...register("message", { required: true })}
-              className={errors.message && classes["input-error"]}
-            />
-            {errors.message && <p>Please provide a message.</p>}
+        <h2 className={classes.title}>Get in touch with me</h2>
+        <ul>
+          <li>
+            <Card className={classes["contact-card"]}>
+              <a
+                className={classes["contact-card-header"]}
+                href="https://www.linkedin.com/in/lance-stasinski/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h3>Connect on LinkedIn</h3>
+              </a>
+            </Card>
+          </li>
+          <li>
+            <Card className={classes["contact-card"]}>
+              <a
+                className={classes["contact-card-header"]}
+                href="https://github.com/LanceStasinski"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h3>View my code on Github</h3>
+              </a>
+            </Card>
+          </li>
+          <li>
+            <Card className={classes["contact-card"]}>
+              <div
+                className={classes["contact-card-header"]}
+                onClick={showFormHandler}
+              >
+                <h3>Send A Message</h3>
+              </div>
+              <CSSTransition
+                in={showForm}
+                timeout={300}
+                mountOnEnter
+                unmountOnExit
+                nodeRef={nodeRef}
+                classNames="slide-down"
+              >
+                <form onSubmit={handleSubmit(onSubmit)} ref={nodeRef}>
+                  <label htmlFor="firstName">First Name</label>
+                  <input
+                    type="text"
+                    id="firstName"
+                    {...register("firstName", { required: true })}
+                    className={errors.firstName && classes["input-error"]}
+                  />
+                  {errors.firstName && <p>Please provide your first name.</p>}
+                  <label htmlFor="lastName">Last Name</label>
+                  <input
+                    type="text"
+                    id="lastName"
+                    {...register("lastName", { required: true })}
+                    className={errors.lastName && classes["input-error"]}
+                  />
+                  {errors.lastName && <p>Please provide your last name.</p>}
+                  <label htmlFor="email">Email</label>
+                  <input
+                    id="email"
+                    type="email"
+                    {...register("email", { required: true })}
+                    className={errors.email && classes["input-error"]}
+                  />
+                  {errors.email && <p>Please provide your email.</p>}
+                  <label htmlFor="message">Message</label>
+                  <textarea
+                    id="message"
+                    {...register("message", { required: true })}
+                    className={errors.message && classes["input-error"]}
+                  />
+                  {errors.message && <p>Please provide a message.</p>}
 
-            <Button type="submit" disabled={!isValid}>
-              SEND
-            </Button>
-          </form>
-        </Card>
+                  <Button type="submit" disabled={!isValid}>
+                    SEND
+                  </Button>
+                </form>
+              </CSSTransition>
+            </Card>
+          </li>
+        </ul>
       </div>
     </React.Fragment>
   );
