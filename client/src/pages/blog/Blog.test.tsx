de1786f7326c1818757c4074e3edb3110d951c37
LanceStasinski <lance.stasinski@gmail.com>
2021-12-21 14:33:38 -0500

    test: Add tests for Blog component

---
+++ client/src/pages/blog/Blog.test.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { render, waitFor, screen } from "@testing-library/react";
+
+import { AuthContext } from "../../shared/context/auth-context";
+import Blog from "./Blog";
+import * as httpHook from "../../shared/hooks/http-hook";
+
+const ADMIN_USER = process.env.REACT_APP_ADMIN_USER;
+
+describe("Blog component", () => {
+  test("renders h2, hr, and link", async () => {
+    const sendRequest = jest.fn(async () => {});
+    jest.spyOn(httpHook, "useHttpClient").mockImplementation(() => {
+      return {
+        isLoading: false,
+        error: undefined,
+        clearError: () => {},
+        sendRequest: sendRequest,
+      };
+    });
+    render(
+      <BrowserRouter>
+        <Route>
+          <AuthContext.Provider
+            value={{
+              isLoggedIn: true,
+              userId: ADMIN_USER!,
+              username: "admin",
+              token: "token",
+              login: (uid, token, username) => {},
+              logout: () => {},
+            }}
+          >
+            <Blog />
+          </AuthContext.Provider>
+        </Route>
+      </BrowserRouter>
+    );
+    const h2Element = document.getElementsByTagName("h2");
+    const hrElement = document.getElementsByTagName("hr");
+    const buttonElement = screen.getByText(/add post/i);
+    expect(h2Element.length).toEqual(2);
+    expect(hrElement.length).toEqual(1);
+    expect(buttonElement).toBeInTheDocument();
+    await waitFor(() => expect(sendRequest).toHaveBeenCalled());
+  });
+});
