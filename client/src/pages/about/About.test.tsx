5ad1e692a176fbb5811788153fadb843c04f9c76
LanceStasinski <lance.stasinski@gmail.com>
2021-12-21 15:06:58 -0500

    test: Add tests for About component

---
+++ client/src/pages/about/About.test.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import About from "./About";
+
+describe("About component", () => {
+  test("renders 2 images, 1 h2, 2 p, and 3 li elements", () => {
+    render(<About />);
+    const headShot = document.getElementsByTagName("img");
+    const background = document.querySelectorAll(
+      'div[class^="background-image"]'
+    );
+    const h2Elements = document.getElementsByTagName("h2");
+    const pElements = document.getElementsByTagName("p");
+    const liElements = document.getElementsByTagName("li");
+    expect(headShot[0]).toBeInTheDocument();
+    expect(background[0]).toBeInTheDocument();
+    expect(h2Elements.length).toEqual(1);
+    expect(pElements.length).toEqual(2);
+    expect(liElements.length).toEqual(3);
+  });
+});
