40b98916912cdc4db218eb10399f5c6565fc7bcd
LanceStasinski <lance.stasinski@gmail.com>
2021-12-22 15:26:10 -0500

    feat: Add social icons to Resume

--- client/src/pages/resume/Resume.tsx
+++ client/src/pages/resume/Resume.tsx
@@ -1,9 +1,66 @@
-import react from 'react';
+import react from "react";
 
-import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import classes from "./Resume.module.css";
+import headshot from "../../assets/headshot-4.jpg";
+import emailIcon from "../../assets/social-icons/email.png";
+import phoneIcon from "../../assets/social-icons/phone.png";
+import locationIcon from "../../assets/social-icons/location.png";
+import globeIcon from "../../assets/social-icons/globe.png";
+import linkedinIcon from "../../assets/social-icons/linkedin2.png";
+import githubIcon from "../../assets/social-icons/github2.png";
+
+const WEBSITE_URL = process.env.REACT_APP_WEBSITE_URL;
 
 const Resume: React.FC = () => {
-  return <LoadingSpinner asOverlay/>
-}
+  return (
+    <div className={classes["resume-wrapper"]}>
+      <div className={classes.page}>
+        <header>
+          <img
+            src={headshot}
+            className={classes.headshot}
+            alt="Headshot of Lance Stasinski"
+          />
+
+          <div>
+            <h2>LANCE</h2>
+            <h2>STASINSKI</h2>
+            <h3>WEB DEVELOPER</h3>
+          </div>
+          <ul className={classes["contact-info"]}>
+            <li>
+              <img src={emailIcon} alt="email icon" />
+              <p>lance.stasinski@gmail.com</p>
+            </li>
+            <li>
+              <img src={phoneIcon} alt="phone icon" />
+              <p>(970) 396-3329</p>
+            </li>
+            <li>
+              <img src={locationIcon} alt="location icon" />
+              <p>Bangor, ME</p>
+            </li>
+            <li>
+              <img src={globeIcon} alt="website icon" />
+              <a href={`${WEBSITE_URL}`}>lancestasinski.com</a>
+            </li>
+            <li>
+              <img src={linkedinIcon} alt="LinkedIn icon" />
+              <a href="https://www.linkedin.com/in/lance-stasinski/">
+                linkedin.com/in/lance-stasinski
+              </a>
+            </li>
+            <li>
+              <img src={githubIcon} alt="Github icon" />
+              <a href="https://github.com/LanceStasinski">
+                github.com/LanceStasinski
+              </a>
+            </li>
+          </ul>
+        </header>
+      </div>
+    </div>
+  );
+};
 
 export default Resume;
