5a47254fc99cae3bf4c12c693cfba3767d5be271
LanceStasinski <lance.stasinski@gmail.com>
2021-12-26 13:47:56 -0500

    feat: Add rounded polygon to header

--- client/src/pages/resume/Resume.tsx
+++ client/src/pages/resume/Resume.tsx
@@ -8,58 +8,81 @@ import locationIcon from "../../assets/social-icons/location.png";
 import globeIcon from "../../assets/social-icons/globe.png";
 import linkedinIcon from "../../assets/social-icons/linkedin2.png";
 import githubIcon from "../../assets/social-icons/github2.png";
+import React from "react";
 
 const WEBSITE_URL = process.env.REACT_APP_WEBSITE_URL;
 
 const Resume: React.FC = () => {
   return (
-    <div className={classes["resume-wrapper"]}>
-      <div className={classes.page}>
-        <header>
-          <img
-            src={headshot}
-            className={classes.headshot}
-            alt="Headshot of Lance Stasinski"
-          />
+    <React.Fragment>
+      <div className={classes["resume-wrapper"]}>
+        <div className={classes.page}>
+          <header>
+            <img
+              src={headshot}
+              className={classes.headshot}
+              alt="Headshot of Lance Stasinski"
+            />
 
-          <div>
-            <h2 className={classes['first-name']}>LANCE</h2>
-            <h2 className={classes['last-name']}>STASINSKI</h2>
-            <h3>WEB DEVELOPER</h3>
-          </div>
-          <ul className={classes["contact-info"]}>
-            <li>
-              <img src={emailIcon} alt="email icon" />
-              <p>lance.stasinski@gmail.com</p>
-            </li>
-            <li>
-              <img src={phoneIcon} alt="phone icon" />
-              <p>(970) 396-3329</p>
-            </li>
-            <li>
-              <img src={locationIcon} alt="location icon" />
-              <p>Bangor, ME</p>
-            </li>
-            <li>
-              <img src={globeIcon} alt="website icon" />
-              <a href={`${WEBSITE_URL}`}>lancestasinski.com</a>
-            </li>
-            <li>
-              <img src={linkedinIcon} alt="LinkedIn icon" />
-              <a href="https://www.linkedin.com/in/lance-stasinski/">
-                linkedin.com/in/lance-stasinski
-              </a>
-            </li>
-            <li>
-              <img src={githubIcon} alt="Github icon" />
-              <a href="https://github.com/LanceStasinski">
-                github.com/LanceStasinski
-              </a>
-            </li>
-          </ul>
-        </header>
+            <div className={classes.title}>
+              <h2 className={classes["first-name"]}>LANCE</h2>
+              <h2 className={classes["last-name"]}>STASINSKI</h2>
+              <h3 className={classes.profession}>WEB DEVELOPER</h3>
+            </div>
+            <ul className={classes["contact-info"]}>
+              <li>
+                <img src={emailIcon} alt="email icon" />
+                <p>lance.stasinski@gmail.com</p>
+              </li>
+              <li>
+                <img src={phoneIcon} alt="phone icon" />
+                <p>(970) 396-3329</p>
+              </li>
+              <li>
+                <img src={locationIcon} alt="location icon" />
+                <p>Bangor, ME</p>
+              </li>
+              <li>
+                <img src={globeIcon} alt="website icon" />
+                <a href={`${WEBSITE_URL}`}>lancestasinski.com</a>
+              </li>
+              <li>
+                <img src={linkedinIcon} alt="LinkedIn icon" />
+                <a href="https://www.linkedin.com/in/lance-stasinski/">
+                  linkedin.com/in/lance-stasinski
+                </a>
+              </li>
+              <li>
+                <img src={githubIcon} alt="Github icon" />
+                <a href="https://github.com/LanceStasinski">
+                  github.com/LanceStasinski
+                </a>
+              </li>
+            </ul>
+          </header>
+        </div>
       </div>
-    </div>
+      {/* <svg
+        style={{visibility: 'hidden', position: 'absolute'}}
+        width="0"
+        height="0"
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+      >
+        <defs>
+          <filter id="goo">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+              result="goo"
+            />
+            <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+          </filter>
+        </defs>
+      </svg> */}
+    </React.Fragment>
   );
 };
 
