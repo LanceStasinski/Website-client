0f1ecde3db2c31baa74bdd9eeb99e68aec28cf34
LanceStasinski <lance.stasinski@gmail.com>
2022-01-04 11:22:25 -0500

    style: Add animation for skill bars

--- client/src/pages/resume/CodingSkill.tsx
+++ client/src/pages/resume/CodingSkill.tsx
@@ -5,19 +5,30 @@ import classes from "./CodingSkill.module.css";
 const CodingSkill: React.FC<{ skillName: string; experience: number }> = (
   props
 ) => {
+  const expand =`
+    @keyframes expand {
+      from {width: 0%;}
+      to {width: 100%;}
+    }
+  `;
   return (
     <li className={classes["coding-skill"]}>
       <p className={classes.skill}>{props.skillName}</p>
       <div className={classes["experience-bar"]}>
-        <div
-          className={classes.experience}
-          style={{ width: `${props.experience}%` }}
-        ></div>
-        <div className={classes.dot}></div>
-        <div
-          className={classes.remainder}
-          style={{ width: `${100 - props.experience}%` }}
-        ></div>
+        <div className={classes["experience-bar-inner"]}>
+          <style children={expand} />
+          <div
+            className={classes["experience-bar-fill"]}
+            style={{
+              maxWidth: `${props.experience}%`,
+              animationName: "expand",
+              animationDuration: "1s",
+              animationTimingFunction: "ease",
+              animationFillMode: "forwards",
+            }}
+          ></div>
+          <div className={classes.dot}></div>
+        </div>
       </div>
     </li>
   );
