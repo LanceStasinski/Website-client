0cf9e79ed3c98e7a79a4396ba62028c64613592e
LanceStasinski <lance.stasinski@gmail.com>
2022-01-18 15:50:05 -0500

    feat: Add website card

--- client/src/pages/portfolio/Portfolio.tsx
+++ client/src/pages/portfolio/Portfolio.tsx
@@ -2,6 +2,9 @@ import React from "react";
 
 import classes from "./Portfolio.module.css";
 import PortfolioCard from "./PortfolioCard";
+import websiteImage from '../../assets/project-images/website.JPG'
+
+
 import fillerImg from '../../assets/headshot-4.jpg'
 
 
@@ -13,12 +16,12 @@ const Portfolio: React.FC = () => {
       <hr />
       <div className={classes.projects}>
         <PortfolioCard
-          image={fillerImg}
+          image={websiteImage}
           gitHubLink="https://github.com/LanceStasinski/Website-client"
           external={true}
           projectUrl="http//:localhost:3000"
-          title="Test1"
-          description="testing testing testing testing testing testing"
+          title="This Website"
+          description="My personal website was is a single page web application that connects to a REST API. The blog portion is a fullstack application in itself. I write individual post using a dynamic form and data is stored in MongoDB and an AWS S3 bucket. Click 'visit' to learn more."
           technologies="TypeScript, React, CSS Modules, Node.js, Express.js, MongoDB, AWS S3"
         />
         <PortfolioCard
