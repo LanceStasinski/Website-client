16d5610744c67943def4455fd0d6429b687fdfe6
LanceStasinski <lance.stasinski@gmail.com>
2022-01-26 15:14:59 -0500

    feat: Add confirm modal to links to projects

--- client/src/pages/portfolio/PortfolioCard.tsx
+++ client/src/pages/portfolio/PortfolioCard.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import classes from "./PortfolioCard.module.css";
 import Card from "../../shared/components/UIElements/Card";
 import githubIcon from "../../assets/social-icons/github2.png";
+import Modal from "../../shared/components/UIElements/Modal";
+import Button from "../../shared/components/FormElements/Button";
 
 const PortfolioCard: React.FC<{
   image: string;
@@ -14,35 +16,71 @@ const PortfolioCard: React.FC<{
   external: boolean;
   projectUrl: string;
 }> = (props) => {
-  return (
-    <Card className={classes['project-card']}>
-      <img
-        className={classes["project-thumbnail"]}
-        src={props.image}
-        alt={props.title}
-      />
-      <div className={classes["project-info"]}>
-        <h2>{props.title}</h2>
-        <p>{props.description}</p>
-        <p><b>Technologies used:</b> {props.technologies}</p>
-      </div>
-      <div className={classes["project-links"]}>
-        {props.external && (
-          <a href={props.projectUrl}>
-            <button>Visit</button>
-          </a>
-        )}
-        {!props.external && (
-          <Link to={props.projectUrl}>
-            <button>Visit</button>
-          </Link>
-        )}
+  const [showModal, setShowModal] = useState(false);
 
-        <a href={props.gitHubLink} className={classes.github}>
-          <img src={githubIcon} alt="Github icon" />
-        </a>
-      </div>
-    </Card>
+  const exitSPAHandler = () => {
+    setShowModal(true);
+  };
+
+  const cancelExitHandler = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Modal
+        show={showModal}
+        onCancel={cancelExitHandler}
+        header="NOTICE: LEAVING SPA"
+        footer={
+          <React.Fragment>
+            <Button href={props.projectUrl}>OKAY</Button>
+            <Button type="button" onClick={cancelExitHandler}>
+              CANCEL
+            </Button>
+          </React.Fragment>
+        }
+        backdropClass={classes["modal-backdrop"]}
+      >
+        Please note: you are leaving the Single Page Application portion of this
+        website.
+      </Modal>
+      <Card className={classes["project-card"]}>
+        <img
+          className={classes["project-thumbnail"]}
+          src={props.image}
+          alt={props.title}
+        />
+        <div className={classes["project-info"]}>
+          <h2>{props.title}</h2>
+          <p>{props.description}</p>
+          <p>
+            <b>Technologies used:</b> {props.technologies}
+          </p>
+        </div>
+        <div className={classes["project-links"]}>
+          {props.external && (
+            <button type="button" onClick={exitSPAHandler}>
+              Visit
+            </button>
+          )}
+          {!props.external && (
+            <Link to={props.projectUrl}>
+              <button>Visit</button>
+            </Link>
+          )}
+
+          <a
+            href={props.gitHubLink}
+            className={classes.github}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={githubIcon} alt="Github icon" />
+          </a>
+        </div>
+      </Card>
+    </React.Fragment>
   );
 };
 
