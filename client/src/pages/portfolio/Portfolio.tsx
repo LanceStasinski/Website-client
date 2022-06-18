d903b047eaf66b1f8476206e308489b0f0f91102
LanceStasinski <lance.stasinski@gmail.com>
2022-06-18 12:48:08 -0400

    feat: Add WakeSkate and State Gov Landing Page portfolio items

--- client/src/pages/portfolio/Portfolio.tsx
+++ client/src/pages/portfolio/Portfolio.tsx
@@ -5,9 +5,11 @@ import PortfolioCard from "./PortfolioCard";
 import websiteImage from "../../assets/project-images/website.JPG";
 import travelAppImage from "../../assets/project-images/travel-app.JPG";
 import nlpAppImage from "../../assets/project-images/NLP-app.JPG";
-import landPageImage from '../../assets/project-images/landing-page.JPG';
-import blogImage from '../../assets/project-images/blog.JPG'
+import landPageImage from "../../assets/project-images/landing-page.JPG";
+import blogImage from "../../assets/project-images/blog.JPG";
 import weatherAppImage from "../../assets/project-images/weather-journal-app.JPG";
+import stateGovLandingPageImage from "../../assets/project-images/state-gov-landing-page.JPG";
+import wakeSkateImage from "../../assets/project-images/wakeskate.JPG";
 
 const SERVER = process.env.REACT_APP_SERVER;
 
@@ -21,11 +23,29 @@ const Portfolio: React.FC = () => {
         <hr />
         <div className={classes.projects}>
           <PortfolioCard
+            image={wakeSkateImage}
+            gitHubLink="https://github.com/LanceStasinski/wakeskate-web"
+            external={true}
+            projectUrl="https://wakeskate.herokuapp.com/"
+            title="WakeSkate?"
+            description="This application rates weather conditions for water skiing given a location chosen by the user. The client allows the user to choose a location from a map, and the UI updates with hourly and daily weather ratings and forecasts. The REST API acts as a middleware between the client and the Google Maps and Open Weather Map APIs."
+            technologies="Ember, Ember Wormhole, JavaScript, TypeScript, Tailwind CSS, Axios, Node, Express"
+          />
+          <PortfolioCard
+            image={stateGovLandingPageImage}
+            gitHubLink="https://github.com/LanceStasinski/PWW-exercise"
+            external={true}
+            projectUrl="https://pww-exercise.web.app/"
+            title="State Government Landing Page"
+            description="This landing page was built from a design artifact and demonstrates my ability to create reusable components and implement pixel-perfect and responsive styling."
+            technologies="TypeScript, React, Tailwind CSS, Jest"
+          />
+          <PortfolioCard
             image={weatherAppImage}
             gitHubLink="https://github.com/LanceStasinski/FEND-03-WeatherJournalApp-React"
             external={true}
             projectUrl={`${SERVER}/weather-journal-app`}
-            title='Weather Journal'
+            title="Weather Journal"
             description="This app generates a journal entry that contains text input by the user and weather data for the selected zip code. The user has access to complete CRUD operations and they can change their zip code or their prefered measurement units at any time."
             technologies="TypeScript, React, styled components, Node/Express, MongoDB, Axios"
           />
@@ -33,7 +53,7 @@ const Portfolio: React.FC = () => {
             image={websiteImage}
             gitHubLink="https://github.com/LanceStasinski/Website-client"
             external={false}
-            projectUrl={'/portfolio/more-info'}
+            projectUrl={"/portfolio/more-info"}
             title="This Website"
             description="My personal website is a single page web application that connects to a REST API. The blog portion is a fullstack application in itself. I write individual posts using a dynamic form and data is stored in MongoDB and an AWS S3 bucket."
             technologies="TypeScript, React, CSS Modules, Node/Express, MongoDB, AWS S3, Jest, Mocha, Chai"
